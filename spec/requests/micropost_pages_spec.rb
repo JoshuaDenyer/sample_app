d0da41a2413e9b91975d050999ce3520c0e316bb
JoshuaDenyer <joshua.denyer@numolabs.com>
2014-01-01 16:35:24 +1100

    A couple of chapter 10 execises

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -7,8 +7,12 @@ describe "Micropost pages" do
   let(:user) { FactoryGirl.create(:user) }
   before { sign_in user }
 
+
+
   describe "micropost creation" do
     before { visit root_path }
+
+    
 
     describe "with invalid information" do
 

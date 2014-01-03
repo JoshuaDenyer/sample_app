adb78e351d17cb824e83dfa771b43a2930ceeb3b
JoshuaDenyer <joshua.denyer@numolabs.com>
2014-01-03 11:50:21 +1100

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -36,11 +36,25 @@ describe "Static pages" do
         end
       end
 
+
+
       describe "when invalid info" do
         before { click_button "Post" }
         it "should not render user feed" do
           expect(page).to have_content('Micropost Feed')
         end
+      end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user))}
+        
       end
     end
   end
